050f2945cc1c3c2f7fe7114631f72a1df9cf5c07
Shambhavi Pandey <shambhavipandeyspvs9@gmail.com>
2026-06-26 15:37:43 +0530

    fix port

--- backend/server.js
+++ backend/server.js
@@ -3,4 +3,4 @@ const connectdb = require("./src/db/db");
 require('dotenv').config();
 
 connectdb();
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Server running"));
